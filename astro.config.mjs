d9dee3ee5773d19c6c8ba1ff6b3770739beb0290
Your Name <namanpatelnpr@gmail.com>
2026-08-01 22:26:34 +0530

    Changed URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://namanlabc.github.io',
-  base: '/my-first-site',
+  site: 'https://naman.blog',
 
   vite: {
     plugins: [tailwindcss()],
