3f8eaca0134c75ec8ad360a64b0f6257a3481b6d
Your Name <namanpatelnpr@gmail.com>
2026-08-01 19:48:02 +0530

    Add Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://namanlabc.github.io',
   base: '/my-first-site',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
